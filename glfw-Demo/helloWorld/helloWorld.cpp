78034e3df5a911b357348d9ed91faa9cf16ff9fd
Jess <SulaFelinae@gmail.com>
2022-01-05 16:08:56 -0800

    test two main functions

---
+++ glfw-Demo/helloWorld/helloWorld.cpp
@@ -0,0 +1,8 @@
+//
+//  helloWorld.cpp
+//  helloWorld
+//
+//  Created by Liang Zhao on 1/4/22.
+//
+
+#include <stdio.h>
---
+++ glfw-Demo/triangle/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  triangle
+//
+//  Created by Liang Zhao on 1/4/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, WorldAAAA !\n";
+    return 0;
+}
