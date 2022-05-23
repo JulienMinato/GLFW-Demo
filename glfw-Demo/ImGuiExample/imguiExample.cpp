ef710cc352bb07862ef005acc847161fa6ec24d6
Liang <SulaFelinae@gmail.com>
2022-05-23 13:59:04 -0700

    add head

--- glfw-Demo/ImGuiExample/imguiExample.cpp
+++ glfw-Demo/ImGuiExample/imguiExample.cpp
@@ -155,9 +155,9 @@ int main(int, char**)
         ImGui::Render();
         int display_w, display_h;
         glfwGetFramebufferSize(window, &display_w, &display_h);
-        glViewport(0, 0, display_w, display_h);
-        glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
-        glClear(GL_COLOR_BUFFER_BIT);
+        //glViewport(0, 0, display_w, display_h);
+        //glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
+        //glClear(GL_COLOR_BUFFER_BIT);
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
         glfwSwapBuffers(window);
---
+++ glfw-Demo/STB_IMAGE/stb_image.cpp
@@ -0,0 +1,8 @@
+//
+//  stb_image.cpp
+//  STB_IMAGE
+//
+//  Created by Liang Zhao on 1/13/22.
+//
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
